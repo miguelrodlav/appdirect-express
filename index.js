b639c10c793f95d6e7ec70fd923b5cc897cbe08e
Miguel Rodriguez <miguel.rodriguez@zerintia.com>
2023-09-27 13:53:03 +0200

    removed auth

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 var qs = require('querystring');
-var apiResponse = require('./api/api-response')
+var apiResponse = require('./api/api-response');
 var ApiHelpers = require('./api/api-helpers.js');
 var ApiAuth = require('./api/api-auth.js');
 
@@ -15,26 +15,39 @@ var controllers = {
   apiController: null,
   subscriptionController: null,
   userController: null,
-  addonController: null
-}
+  addonController: null,
+};
 
 function getControllers() {
   return {
     apiController: require(path.join(config.controllers, 'api.js')),
-    subscriptionController: require(path.join(config.controllers, 'subscription.js')),
+    subscriptionController: require(path.join(
+      config.controllers,
+      'subscription.js'
+    )),
     userController: require(path.join(config.controllers, 'user.js')),
-    addonController: require(path.join(config.controllers, 'addon.js'))
-  }
+    addonController: require(path.join(config.controllers, 'addon.js')),
+  };
 }
 
-module.exports = function(app, options) {
+module.exports = function (app, options) {
   config = options;
   // Check  the correct congif options have been passed
-  if(!config) throw new Exception("Options must be passed to the appdirect-express module")
-  if(!app) throw new Exception("An express app must be passwed to the appdirect-express module")
+  if (!config)
+    throw new Exception(
+      'Options must be passed to the appdirect-express module'
+    );
+  if (!app)
+    throw new Exception(
+      'An express app must be passwed to the appdirect-express module'
+    );
 
-  if(config.use_mocks) config.controllers = path.join(__dirname, '/mock_controllers');
-  else if(!config.controllers) throw new Exception("You must provide a controllers directory when use_mocks is off")
+  if (config.use_mocks)
+    config.controllers = path.join(__dirname, '/mock_controllers');
+  else if (!config.controllers)
+    throw new Exception(
+      'You must provide a controllers directory when use_mocks is off'
+    );
 
   // Map the controllers for use later
   controllers = getControllers();
@@ -50,34 +63,46 @@ module.exports = function(app, options) {
   /**
    * Route middleware to validate oauth signature
    */
-  baseApi.use('/', apiAuth.requireAuth);
+  //baseApi.use('/', apiAuth.requireAuth);
 
   /**
    * Route middleware to validate that event url is present
    */
-  baseApi.use('/', apiHelpers.checkForEventUrl)
+  baseApi.use('/', apiHelpers.checkForEventUrl);
 
   /**
    * Route middleware to get event data from the request
    */
-  baseApi.use('/', apiHelpers.getEventData)
-  baseApi.use('/subscription', require('./routes/subscription.js')(controllers.subscriptionController, config));
-  baseApi.use('/user', require('./routes/user.js')(controllers.userController, config))
-  baseApi.use('/addon', require('./routes/addon.js')(controllers.addonController, config))
+  baseApi.use('/', apiHelpers.getEventData);
+  baseApi.use(
+    '/subscription',
+    require('./routes/subscription.js')(
+      controllers.subscriptionController,
+      config
+    )
+  );
+  baseApi.use(
+    '/user',
+    require('./routes/user.js')(controllers.userController, config)
+  );
+  baseApi.use(
+    '/addon',
+    require('./routes/addon.js')(controllers.addonController, config)
+  );
   app.use('/api', baseApi);
 
   /**
    * Catch 404 and forward to error handler
    */
-  baseApi.use(function(req, res, next) {
+  baseApi.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
   });
 
-  return function(err, req, res, next) {
+  return function (err, req, res, next) {
     console.log(err);
     console.log(err.stack);
     return res.sendApiError('UNKNOWN_ERROR', err.message);
-  }
-}
+  };
+};
